6d6a4f6a4bdf1fe626d4668a4a21cce1d94d4448
SeungWookHan <hswook12@me.com>
2020-10-05 00:35:06 +0900

    로그인 기능 샘플 추가

--- restfulapi/urls.py
+++ restfulapi/urls.py
@@ -21,5 +21,7 @@ from django.urls import path
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('addresses/', views.address_list),
+    path('addresses/<int:pk>/', views.address),
+    path('login/', views.login),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
