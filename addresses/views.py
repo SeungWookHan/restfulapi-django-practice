b8b0475e7a9ef3abdc019555426d71bad52e20c4
SeungWookHan <hswook12@me.com>
2020-10-20 01:35:25 +0900

    Addresses model에 GET, POST 성공

---
+++ addresses/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from . models import Addresses
+from . serializers import AddressSerializer
+
+
+@csrf_exempt
+def address_list(request):
+    if request.method == 'GET':
+        queryset = Addresses.objects.all()
+        serializer = AddressSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = AddressSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
--- restfulapi/settings.py
+++ restfulapi/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'addresses.apps.AddressesConfig',
 ]
 
 REST_FRAMEWORK = {
